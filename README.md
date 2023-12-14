619e207e1e8d2b48d74c07c95c37980a1f783956
Frankie Primerano <frankieprimerano@gmail.com>
2023-12-14 12:56:10 -0500

    initial multithreaded implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# No Checkin
+
+This program walks a given directory and searches for the `NOCHECKIN` keyword. If found, it exists with a non-zero status code. Otherwise it exists with a zero status code. This can be used in a git pre-commit hook.
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use std::{env, process::exit, fs::{self, read_dir, DirEntry}, sync::mpsc};
+use std::thread;
+
+fn does_file_contain_nocheckin(file: &str, nocheckin_str: &str) -> bool {
+    let file_contents = fs::read_to_string(file);
+    if let Ok(x) = file_contents {
+        return x.contains(&nocheckin_str)
+    }
+
+    false
+}
+
+fn apply_filter(f: &DirEntry) -> bool {
+    f.file_name()
+    .to_str()
+    .map(|x| x.starts_with("."))
+    .unwrap_or(false)
+}
+
+fn walk_dir(path: &str) -> (Vec<String>, bool) {
+    let dir = read_dir(path);
+
+    let (tx, rx) = mpsc::channel();
+
+    let mut contains_nocheckin: Vec<String> = Vec::new();
+
+    if let Ok(dir) = dir {
+        thread::scope(|s| {
+            for x in dir {
+                let ctx = tx.clone();
+                if let Ok(x) = x {
+                    if apply_filter(&x) {
+                        continue;
+                    }
+
+                    let fp = x.path();
+
+                    if x.path().is_dir() {
+                        s.spawn(move || {
+                            let (r, ncf) = walk_dir(&fp.to_str().unwrap());
+                        
+                            ctx.send(ncf).unwrap();
+                        
+                            for nc in r {
+                                println!("{} contains no checkin", nc); 
+                            }
+                        });
+                    } else if does_file_contain_nocheckin(&fp.to_str().unwrap(), "NOCHECKIN") {
+                        contains_nocheckin.push(String::from(fp.to_str().unwrap()));
+                        ctx.send(true).unwrap();
+                    }
+                }
+            }
+        });
+    }
+
+    drop(tx);
+
+    for r in rx {
+        if r {
+            return (contains_nocheckin, true)
+        }
+    }
+
+    (contains_nocheckin, false)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let arg = args.get(1);
+
+    if let Some(path) = arg {
+        let (_, ncf) = walk_dir(path);
+        if ncf {
+            println!("No checkin found.");
+            exit(1)
+        }
+    } else {
+        println!("Usage: ./nocheckin <root>");
+        exit(1)
+    }
+
+}
